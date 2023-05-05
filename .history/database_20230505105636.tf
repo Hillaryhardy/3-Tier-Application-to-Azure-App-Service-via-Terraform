18211948c3369556f9ce2b08df6d24747fdcf4b8
hillary <kibet_hillary@yahoo.com>
2023-05-05 10:57:00 +0300

    database configs

---
+++ .history/database_20230505105636.tf
@@ -0,0 +1,35 @@
+
+#Create Random password 
+resource "random_password" "randompassword" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+#Create Key Vault Secret
+resource "azurerm_key_vault_secret" "sqladminpassword" {
+  # checkov:skip=CKV_AZURE_41:Expiration not needed 
+  name         = "sqladmin"
+  value        = random_password.randompassword.result
+  key_vault_id = azurerm_key_vault.fg-keyvault.id
+  content_type = "text/plain"
+  depends_on = [
+    azurerm_key_vault.fg-keyvault,azurerm_key_vault_access_policy.kv_access_policy_01,azurerm_key_vault_access_policy.kv_access_policy_02,azurerm_key_vault_access_policy.kv_access_policy_03
+  ]
+}
+
+#Azure sql database
+resource "azurerm_mssql_server" "azuresql" {
+  name                         = "fg-sqldb-prod"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "4adminu$er"
+  administrator_login_password = random_password.randompassword.result
+
+  azuread_administrator {
+    login_username = "AzureAD Admin"
+    object_id      = "86f50fc0-0d0d-4c26-941d-17dd64ed03a6"
+  }
+}
+
